c27c337def77aed2f3a22be008c852363932eedf
Your Name <akhemraiev@luxoft.com>
2020-02-23 22:55:59 +0200

    - adds plotting

--- iris.py
+++ iris.py
@@ -1,5 +1,8 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
+import mglearn
 
 iris_dataset = load_iris()
 
@@ -18,3 +21,7 @@ X_train, X_test, y_train, y_test = \
     train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
 
 print(len(X_train))
+
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=0.8, cmap=mglearn.cm3)
+plt.show()
